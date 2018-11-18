efdf3c74c4ab07487ea380b2c11088e3636b0b28
usaches <usaches@gmail.com>
2018-11-17 19:43:17 -0500

    css pls

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type: "application/vnd.api+json"}));
+app.use(express.static(__dirname + "/app"));
 require("./app/routing/apiRoutes.js")(app);
 require("./app/routing/htmlRoutes.js")(app);
 app.listen(PORT, function () {
